7729b014b3a5e9142cc65b62ba4bfb181ef62c58
lowks <kianseong@gmail.com>
2015-03-29 14:42:17 +0800

    Changing elixir-convert into package by adding mix.exs

---
+++ mix.exs
@@ -0,0 +1,43 @@
+defmodule Convert.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :Convert,
+     version: "0.0.1",
+     elixir: "~> 1.0",
+     package: package,
+     description: "A library to do numeric base convertion",
+     deps: deps]
+  end
+
+  defp package do
+	 [
+		files: ["lib", "mix.exs", "readme.md"], 
+		contributors: ["Spike Grobstein"],
+		licenses: ["MIT"],
+		links: %{
+			"GitHub" => "https://github.com/spikegrobstein/elixir-convert",
+		}
+	]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type `mix help compile.app` for more information
+  def application do
+    [applications: [:logger]]
+  end
+
+  # Dependencies can be Hex packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #
+  # Type `mix help deps` for more examples and options
+  defp deps do
+    []
+  end
+end
