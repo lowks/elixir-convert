0e8344e607b843b55294b4866a9e3fe227762ad0
lowks <kianseong@gmail.com>
2015-03-29 14:08:12 +0800

    Fix convert.ex to latest elixir language changes

--- convert.ex
+++ convert.ex
@@ -51,10 +51,10 @@ defmodule Convert do
     # eg: from_binary_string("1010011010") -> 666
     def from_binary( value ) when is_bitstring( value ) do
 
-      bin_list = String.split( value, %r{} )             # split the string into characters
-                  |> filter( &1 != "" )                  # drop any chars that are empty strings (String.split terminates the list with an empty string for some reason)
-                  |> map( String.to_integer(&1) )        # convert each one into an int; this returns tuples of {i, ""}
-                  |> map( fn(i) -> { n, _ } = i; n end ) # get the value from each tuple
+      bin_list = String.split( value, ~r{} )             # split the string into characters
+                  |> Enum.filter( fn(x) -> x != "" end)                  # drop any chars that are empty strings (String.split terminates the list with an empty string for some reason)
+                  |> Enum.map( fn(x) -> String.to_integer(x) end)        # convert each one into an int; this returns tuples of {i, ""}
+                  |> Enum.map( fn(i) -> { n, _ } = i; n end ) # get the value from each tuple
 
       # now convert that list into an int using the main function
       from_binary bin_list
@@ -76,13 +76,13 @@ defmodule Convert do
     import Math
 
     # lookup table for hex values
-    @lookup_table %w{ 0 1 2 3 4 5 6 7 8 9 A B C D E F }
+    @lookup_table ~w( 0 1 2 3 4 5 6 7 8 9 A B C D E F )
 
     # given a value as a positive integer
     # return a list of string values containing the hex characters
     def from_dec( value ) when value > 0 do
       do_from_dec( value , [] )         # convert to list of integers (0-15)
-        |> map( at(@lookup_table, &1) ) # translate integers into 0-F values from @lookup_table
+        |> Enum.map( fn(x) -> at(@lookup_table, x) end ) # translate integers into 0-F values from @lookup_table
     end
 
     # given a value as a positive integer
@@ -109,8 +109,8 @@ defmodule Convert do
     # Given a string representation of a hex value,
     # return the integer representation of it
     def from_hex( value ) when is_bitstring( value ) do
-      hex_list = String.split( value, %r{} )
-                  |> filter( &1 != "" )
+      hex_list = String.split( value, ~r{} )
+                  |> Enum.filter( fn(x) -> x != "" end)
 
       from_hex( hex_list )
     end
